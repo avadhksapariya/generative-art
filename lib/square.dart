e992bf2a631cacc37083f578681a5ad6fbba5d78
EmpiereTech-Avadh <developer.empiere@gmail.com>
2025-04-02 18:45:13 +0530

    - Square : Added customPainter to draw the Square with paint object and drawRect API of Flutter.

---
+++ lib/square.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Square extends StatelessWidget {
+  const Square({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ColoredBox(color: Colors.white, child: SizedBox.expand(child: CustomPaint()));
+  }
+}
+
+class SquareCustomPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint =
+        Paint()
+          ..color = Colors.black
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 3.0;
+
+    final center = Offset(size.width / 2, size.height / 2);
+
+    final side = size.shortestSide * 0.8;
+
+    canvas.drawRect(Rect.fromCenter(center: center, width: side, height: side), paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
