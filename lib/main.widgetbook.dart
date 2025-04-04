003e685c33adc527df11d7a8432cc0737c820fa6
EmpiereTech-Avadh <developer.empiere@gmail.com>
2025-04-04 14:53:31 +0530

    - Recursive Square Grid: Added squares grid rendering horizontal and vertical recursively for available measures - Rendered squares grid with or without color and other parameters of strokeWidth, gap, saturation and lightness etc.

--- lib/main.widgetbook.dart
+++ lib/main.widgetbook.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:generative_art/styles/themes.dart';
+import 'package:generative_art/vera_molnar/recursive_squares_grid.dart';
 import 'package:generative_art/vera_molnar/square.dart';
 import 'package:generative_art/vera_molnar/squares_grid.dart';
 import 'package:widgetbook/widgetbook.dart';
@@ -54,6 +55,51 @@ class WidgetBookApp extends StatelessWidget {
                           width: MediaQuery.of(context).size.width,
                           height: MediaQuery.of(context).size.height,
                           child: SquaresGrid(),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+                WidgetbookComponent(
+                  name: "RecursiveSquareGrid",
+                  useCases: [
+                    WidgetbookUseCase(
+                      name: "Playground",
+                      builder: (context) {
+                        return SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          height: MediaQuery.of(context).size.height,
+                          child: RecursiveSquaresGrid(
+                            enableColors: context.knobs.boolean(label: 'Enable Colors', initialValue: true),
+                            strokeWidth: context.knobs.double.slider(
+                              label: 'Stroke Width',
+                              initialValue: 1.0,
+                              min: 0.5,
+                              max: 3.5,
+                              divisions: 6,
+                            ),
+                            side: context.knobs.double.slider(
+                              label: 'Square Side Length',
+                              initialValue: 80,
+                              min: 30,
+                              max: 200,
+                            ),
+                            gap: context.knobs.double.slider(label: 'Gap', initialValue: 5, min: 5, max: 50),
+                            saturation: context.knobs.double.slider(
+                              label: 'Saturation',
+                              initialValue: 0.7,
+                              min: 0,
+                              max: 1.0,
+                              divisions: 10,
+                            ),
+                            lightness: context.knobs.double.slider(
+                              label: 'Lightness',
+                              initialValue: 0.5,
+                              min: 0,
+                              max: 1.0,
+                              divisions: 10,
+                            ),
+                          ),
                         );
                       },
                     ),
---
+++ lib/vera_molnar/recursive_squares_grid.dart
@@ -0,0 +1,137 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class RecursiveSquaresGrid extends StatelessWidget {
+  const RecursiveSquaresGrid({
+    super.key,
+    this.side = 80,
+    this.strokeWidth = 2,
+    this.gap = 5,
+    this.saturation = 0.7,
+    this.lightness = 0.5,
+    this.minSquareSideFraction = 0.2,
+    this.enableColors = true,
+  });
+
+  final double side;
+  final double strokeWidth;
+  final double gap;
+  final double saturation;
+  final double lightness;
+  final double minSquareSideFraction;
+  final bool enableColors;
+
+  @override
+  Widget build(BuildContext context) {
+    return ColoredBox(
+      color: Colors.white,
+      child: SizedBox.expand(
+        child: CustomPaint(
+          painter: CSRecursiveSquaresGrid(
+            sideLength: side,
+            strokeWidth: strokeWidth,
+            gap: gap,
+            saturation: saturation,
+            lightness: lightness,
+            enableColors: enableColors,
+            minSquareSideFraction: minSquareSideFraction,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CSRecursiveSquaresGrid extends CustomPainter {
+  CSRecursiveSquaresGrid({
+    super.repaint,
+    this.sideLength = 80,
+    this.strokeWidth = 2,
+    this.gap = 10,
+    this.saturation = 0.7,
+    this.lightness = 0.5,
+    this.minSquareSideFraction = 0.2,
+    this.enableColors = true,
+  }) : minSideLength = sideLength * minSquareSideFraction;
+
+  final double sideLength;
+  final double strokeWidth;
+  final double gap;
+  final double minSideLength;
+  final double saturation;
+  final double lightness;
+  final double minSquareSideFraction;
+  final bool enableColors;
+
+  static final Random random = Random();
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint =
+        Paint()
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = strokeWidth;
+
+    // Calculate the number of squares that can fit on the horizontal axis
+    final xCount = ((size.width + gap) / (sideLength + gap)).floor();
+
+    // Calculate the number of squares that can fit on the vertical axis
+    final yCount = ((size.height + gap) / (sideLength + gap)).floor();
+
+    // Calculate the size of the grid of squares
+    final contentSize = Size(
+      (xCount * sideLength) + ((xCount - 1) * gap),
+      (yCount * sideLength) + ((yCount - 1) * gap),
+    );
+
+    // Calculate the offset from which to start painting
+    // the grid so that it us eventually centered.
+    final offset = Offset((size.width - contentSize.width) / 2, (size.height - contentSize.height) / 2);
+
+    final totalCount = xCount * yCount;
+
+    canvas.save();
+    canvas.translate(offset.dx, offset.dy);
+
+    final depth = random.nextInt(5) + 5;
+
+    for (int index = 0; index < totalCount; index++) {
+      int i = index ~/ yCount;
+      int j = index % yCount;
+
+      // Recursively draw squares
+      drawNestedSquare(canvas, Offset((i * (sideLength + gap)), (j * (sideLength + gap))), sideLength, paint, depth);
+    }
+    canvas.restore();
+  }
+
+  void drawNestedSquare(Canvas canvas, Offset start, double sideLength, Paint paint, int depth) {
+    // Recursively draw squares until the side of the square
+    // reaches the minimum defined by the 'minSideLength' input
+    // or until the 'depth' reaches 0
+    if (sideLength < minSideLength || depth <= 0) return;
+
+    if (enableColors) {
+      paint.color = HSLColor.fromAHSL(1, random.nextInt(360).toDouble(), saturation, lightness).toColor();
+    }
+
+    canvas.drawRect(Rect.fromLTWH(start.dx, start.dy, sideLength, sideLength), paint);
+
+    // calculate the side length for the next square randomly
+    final nextSideLength = sideLength * (random.nextDouble() * 0.5 + 0.5);
+
+    final nextStart = Offset(
+      start.dx + sideLength / 2 - nextSideLength / 2,
+      start.dy + sideLength / 2 - nextSideLength / 2,
+    );
+
+    // recursive call with the next side length and starting point & 'depth'
+    drawNestedSquare(canvas, nextStart, nextSideLength, paint, depth - 1);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
